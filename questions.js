3b4bf72389671b475bbbab88df4f8d6644b96a3d
unknown <kyleward1992@gmail.com>
2020-09-30 02:19:20 -0400

    final revisions to code quiz

---
+++ questions.js
@@ -0,0 +1,29 @@
+var questions = [
+    {title: "Which of the following is true about variable naming conventions in JavaScript?",
+        choices: ["JavaScript variable names must begin with a letter or the underscore character", "JavaScript variable names are case sensitive", "Both of the above", "None of the above"],
+        answer: "Both of the above"},
+   
+    {title: "Which of the following is the correct syntax to redirect a url using JavaScript?",
+        choices: ["document.location='http://www.newlocation.com'", "browser.location='http://www.newlocation.com'", "navigator.location='http://www.newlocation.com'", "window.location='http://www.newlocation.com'"],
+        answer: "window.location='http://www.newlocation.com'"},
+
+    {title: "Which built-in method calls a function for each element in the array?",
+        choices: ["while()", "loop()", "forEach()", "None of the above"],
+        answer: "forEach()"},
+
+    {title: "Which built-in method reverses the order of the elements of an array?",
+        choices: ["changeOrder(order)", "reverse()",  "sort(order)",  "None of the above"],
+        answer: "reverse()"},
+
+    {title: "Which of the following function of Boolean object returns a string containing the source of the Boolean object?",
+        choices: ["toSource()", "valueOf()", "toString()", "None of the above"],
+        answer: "toSource()"},
+    
+    {title: "Which of the following function of String object returns the index within the calling String object of the last occurrence of the specified value?",
+        choices: ["lastIndexOf()", "search()", "substr()", "indexOf()"],
+        answer: "lastIndexOf()"},
+
+
+  
+
+];
